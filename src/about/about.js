0a6f2f1104158d4de60f96839a8ce7de15c4ee47
cheru <30118310+DennoCheru@users.noreply.github.com>
2026-01-04 16:10:14 +0300

    Add About Us, contact us section

--- src/about/about.js
+++ src/about/about.js
@@ -12,7 +12,67 @@ Kericho, Kenya.
 locationContainer.appendChild(locationTitle);
 locationContainer.appendChild(locationText)
 
-container.appendChild(locationContainer)
+const contactUs = document.createElement('div');
+const contactTitle = document.createElement('h1');
+contactTitle.textContent = "Contact Us";
+contactUs.appendChild(contactTitle);
+
+const contactForm = document.createElement('form');
+contactForm.classList.add('contactForm')
+const fields = [
+    {
+        name: "nameField",
+        inputType: "input",
+        type: "text",
+        id : "name",
+        placeholder: "Your Name here..."
+    },
+    {
+        name: "emailField",
+        inputType: "input",
+        type: "email",
+        id: "email",
+        placeholder: "Your Email here",
+    },
+    {
+        name: "messageField",
+        inputType: "textarea",
+        id: "message",
+        placeholder: "Your Message here..."
+    },
+];
+
+fields.forEach(field => {
+    const fieldWrapper = document.createElement('div');
+    fieldWrapper.classList.add('formField');
+
+    const element = document.createElement(field.inputType);
+    element.id = field.id;
+    element.name = field.name;
+    element.placeholder = field.placeholder;
+
+    if (field.inputType === "input") {
+        element.type = field.type;
+    };
+
+    if (field.text) {
+        element.textContent = field.text;
+    }
+
+    fieldWrapper.appendChild(element);
+    contactForm.appendChild(fieldWrapper);
+});
+const submitBtn = document.createElement('button');
+submitBtn.type = "submit";
+submitBtn.textContent = "Send Message";
+contactForm.appendChild(submitBtn);
+
+contactUs.appendChild(contactForm);
+
+contactForm.appendChild(submitBtn)
+
+container.appendChild(locationContainer);
+container.appendChild(contactUs);
 }
 
 export default About;
