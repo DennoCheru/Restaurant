878007df5dd09225b71819f7de0fce2066127bc1
cheru <30118310+DennoCheru@users.noreply.github.com>
2026-01-04 18:03:23 +0300

    Add navigation to desired pages

--- src/index.js
+++ src/index.js
@@ -3,4 +3,46 @@ import Home from "./home/home.js";
 import Services from "./services/services.js";
 import About from "./about/about.js";
 
-Services();
+function Load() {
+    const container = document.querySelector('.container');
+    container.textContent = "";
+    Home();
+
+    const nav = document.querySelector('nav');
+
+    nav.addEventListener('click', (e)=> {
+        nav.querySelectorAll('button').forEach(btn => 
+            btn.classList.remove('active')
+        );
+
+        e.target.classList.add('active');
+        
+        if (!e.target.matches('button[data-page]')) return;
+
+        const page = e.target.dataset.page;
+
+        switch(page) {
+            case 'home':
+                clearContent();
+                Home();
+                break;
+            case 'services':
+                clearContent();
+                Services();
+                break;
+            case 'about':
+                clearContent()
+                About()
+                break;
+            default:
+                clearContent();
+                container.textContent = "404 Error! Page not found."
+        }
+    });
+
+    function clearContent() {
+        container.textContent = "";
+    }
+}
+
+Load();
